dfc3772352ca3d172b044283ff625d053fd6b850
asmaembr <asmae.moubr@gmail.com>
2024-12-20 00:47:05 +0100

    workspace folder cleaned

---
+++ zuul-server/src/main/java/ma/commande/zuul_server/ZuulServerApplication.java
@@ -0,0 +1,13 @@
+package ma.commande.zuul_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+}
---
+++ zuul-server/src/test/java/ma/commande/zuul_server/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.commande.zuul_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
