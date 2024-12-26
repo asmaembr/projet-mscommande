a24499d653a87b5ae54c5dcdc688c1edb2b50d4d
asmaembr <asmae.moubr@gmail.com>
2024-12-26 16:29:13 +0100

    testing commande configuration property

--- microservice-commandes/src/main/java/ma/commande/microservicecommandes/MicroserviceCommandesApplication.java
+++ microservice-commandes/src/main/java/ma/commande/microservicecommandes/MicroserviceCommandesApplication.java
@@ -2,16 +2,12 @@ package ma.commande.microservicecommandes;
 
 import ma.commande.microservicecommandes.model.Commande;
 import ma.commande.microservicecommandes.repository.CommandeRepository;
-import ma.commande.microservicecommandes.service.CommandeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.util.stream.IntStream;
@@ -21,11 +17,23 @@ import java.util.stream.IntStream;
 @SpringBootApplication
 public class MicroserviceCommandesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceCommandesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceCommandesApplication.class, args);
+    }
 
-
-
+    @Bean
+    public CommandLineRunner initData(CommandeRepository repository) {
+        return args -> {
+            IntStream.rangeClosed(1, 40).forEach(i -> {
+                Commande commande = Commande.builder()
+                        .description("Commande " + i)
+                        .quantite(i * 10)
+                        .date(LocalDate.now().minusDays(i))
+                        .montant(100.0 + i * 10.0)
+                        .build();
+                repository.save(commande);
+            });
+        };
+    }
 
 }
